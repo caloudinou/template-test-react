ba2647271f6b5d20b3da53060a6cba542a07bfe7
caloudinou <pascalou@gmail.com>
2022-01-31 03:11:59 +0100

    Create StyledButton.js

---
+++ src/StyledButton.js
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+const StyledButton = styled.button`
+  height: 40px;
+  width: 200px;
+  border-radius: 10px;
+  border: none;
+  background-color: ${({ isOn, isReset }) =>
+    isOn ? "red" : isReset ? "mistyrose" : "green"};
+  margin-top: ${({ isReset }) => (isReset ? "30px" : "10px")};
+`;
+
+export default StyledButton;
